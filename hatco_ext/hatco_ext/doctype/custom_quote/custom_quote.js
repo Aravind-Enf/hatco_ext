4a256452c633c06b50e8fec99d48defe6e52a983
roshmol <roshnatnbr@gmail.com>
2026-02-04 09:25:22 +0000

    feat:add a script for set valid till date

--- hatco_ext/hatco_ext/doctype/custom_quote/custom_quote.js
+++ hatco_ext/hatco_ext/doctype/custom_quote/custom_quote.js
@@ -1,8 +1,28 @@
 // Copyright (c) 2026, Aravind R and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Custom Quote", {
-// 	refresh(frm) {
 
-// 	},
-// });
+//Set valid till date to 30 days from posting date
+frappe.ui.form.on("Custom Quote", {
+    onload(frm) {
+        if (frm.is_new()) {
+            if (!frm.doc.posting_date) {
+                frm.set_value(
+                    "posting_date",
+                    frappe.datetime.now_datetime()
+                );
+            }
+            if (!frm.doc.valid_till) {
+                let posting_date = frm.doc.posting_date || frappe.datetime.now_datetime();
+
+                let valid_till = frappe.datetime.add_days(
+                    posting_date,
+                    30
+                );
+
+                frm.set_value("valid_till", valid_till);
+            }
+        }
+    }
+});
+
